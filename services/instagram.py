c0099dd97e0e57eb8f6a5ca5434b4f189a9f25cf
zaker0753-hub <zaker0753@gmail.com>
2026-06-12 15:56:20 +0330

    Update instagram.py

--- services/instagram.py
+++ services/instagram.py
@@ -6,7 +6,7 @@ from yt_dlp import YoutubeDL
 
 def get_instagram_info(url):
 
-    ydl_opts = {"quiet": True}
+    ydl_opts = {"quiet": True, "cookiefile": "cookiesins.txt"}
 
     with YoutubeDL(ydl_opts) as ydl:
 
@@ -26,7 +26,7 @@ def download_instagram(url, user_id):
 
     output = f"downloads/" f"{user_id}_{unique_id}_%(playlist_index)s.%(ext)s"
 
-    ydl_opts = {"outtmpl": output, "quiet": True, "merge_output_format": "mp4"}
+    ydl_opts = {"outtmpl": output, "quiet": True, "merge_output_format": "mp4", "cookiefile": "cookiesins.txt"}
 
     downloaded_files = []
 
